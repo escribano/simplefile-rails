e2d5753c8ba85e4525bef29f2c09219a28a5ed1c
Jamie Roth <roth.j@me.com>
2013-11-14 01:41:07 -0500

    begin to rebuild statlanding page

--- simplefile/config/routes.rb
+++ simplefile/config/routes.rb
@@ -1,5 +1,5 @@
 Simplefile::Application.routes.draw do
-  get "pages#home"
+  root "pages#home"
   get "signup" => "pages#signup"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
